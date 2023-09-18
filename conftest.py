ef757531d6d5cd022589ee2064c3b56b12c8615e
ahranat <granatov58@gmail.com>
2023-09-18 17:13:16 +0200

    added test, env, ini,

---
+++ conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose lang")
+    
+@pytest.fixture(scope='function')
+def browser(request):
+    user_language = request.config.getoption('language')
+    print("\nstart browser for test..")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
