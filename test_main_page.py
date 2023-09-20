36b80c819df02c6c1669fd8e2e322094f10cf963
ahranat <granatov58@gmail.com>
2023-09-20 11:32:00 +0200

    added test_guest_can_go_to_login

--- test_main_page.py
+++ test_main_page.py
@@ -2,14 +2,16 @@ import pytest
 from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from .pages.login_page import LoginPage
 
-link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
-
+link = "http://selenium1py.pythonanywhere.com"
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)   
-    page.open()                      
-    # page.go_to_login_page()          
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()         
 
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
