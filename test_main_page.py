c7054aade08e942275b0fc7e046b8df764c1baeb
ahranat <granatov58@gmail.com>
2023-09-20 10:56:40 +0200

    added login_page and locators for login_page

--- test_main_page.py
+++ test_main_page.py
@@ -3,15 +3,16 @@ from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
-link = "http://selenium1py.pythonanywhere.com/"
+link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
 
 
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   
     page.open()                      
-    page.go_to_login_page()          
+    # page.go_to_login_page()          
 
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
