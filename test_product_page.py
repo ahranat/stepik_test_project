b70b7fc8cc498a90373477447344c8a4861b2d23
ahranat <granatov58@gmail.com>
2023-09-20 15:07:20 +0200

    added price test and name test

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,6 @@
 import pytest 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from .pages.product_page import ProductPage
+import time
 
 
 link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear'
@@ -10,10 +9,12 @@ link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-ha
 def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
+    page.should_be_New_Year_in_url()
     page.should_be_add_to_cart_button()
     page.add_to_cart()
     page.solve_quiz_and_get_code()
-
+    page.should_be_message_about_adding()
+    page.should_be_message_basket_total()
 
 if __name__ == '__main__':
     pytest.main()
