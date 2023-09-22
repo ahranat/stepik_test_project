40fe8f9650afd1a9f938cf840dd9eb309a1f5873
ahranat <granatov58@gmail.com>
2023-09-22 16:21:36 +0200

    aadded products page locators

--- pages/product_page.py
+++ pages/product_page.py
@@ -10,20 +10,17 @@ class ProductPage(BasePage):
     def add_to_cart(self):
         add_to_cart = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
         add_to_cart.click()
-    
-    def should_be_New_Year_in_url(self):
-        assert 'newYear' in self.browser.current_url, 'Login is not presented in url'
 
     def should_be_message_about_adding(self):
         assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), "Product name is not presented"
         assert self.is_element_present(*ProductPageLocators.MESSAGE_ABOUT_ADDING), "Message about adding is not presented"
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         message = self.browser.find_element(*ProductPageLocators.MESSAGE_ABOUT_ADDING).text
-        assert product_name in message, "No product name in the message"
+        assert product_name == message, "No product name in the message"
         
     def should_be_message_basket_total(self):
         assert self.is_element_present(*ProductPageLocators.MESSAGE_BASKET_TOTAL), "Message basket total is not presented"
         assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), "Product price is not presented"
         message_basket_total = self.browser.find_element(*ProductPageLocators.MESSAGE_BASKET_TOTAL).text
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
-        assert product_price in message_basket_total, "No product price in the message"
+        assert product_price == message_basket_total, "No product price in the message"
--- test_product_page.py
+++ test_product_page.py
@@ -3,13 +3,21 @@ from .pages.product_page import ProductPage
 import time
 
 
-link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-
-
-def test_guest_can_add_product_to_basket(browser):
+links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+        pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail(reason="Won't fix")),
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+        "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+        ]
+@pytest.mark.parametrize('link', links)
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
-    page.should_be_New_Year_in_url()
     page.should_be_add_to_cart_button()
     page.add_to_cart()
     page.solve_quiz_and_get_code()
