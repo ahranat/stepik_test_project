4426744e783be247d8db66254fe36e6a8f63d300
ahranat <granatov58@gmail.com>
2023-09-25 13:12:49 +0200

    added negative tests

--- test_product_page.py
+++ test_product_page.py
@@ -24,5 +24,28 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_message_about_adding()
     page.should_be_message_basket_total()
 
+class TestNegative:
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    
+    def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser):
+        page = ProductPage(browser, self.link)
+        page.open()
+        page.add_to_cart()
+        page.solve_quiz_and_get_code()
+        page.should_not_be_success_message()
+
+    def test_guest_cant_see_success_message(self, browser):
+        page = ProductPage(browser, self.link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_message_disappeared_after_adding_product_to_basket(self, browser):
+        page = ProductPage(browser, self.link)
+        page.open()
+        page.add_to_cart()
+        page.solve_quiz_and_get_code()
+        page.should_disappear_success_message()
+
+
 if __name__ == '__main__':
     pytest.main()
